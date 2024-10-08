67da5a654ae9f55ff322cb835bbcadcf9cb2fbcf
Rajesh Raj <rajeshsapbtp225@gmail.com>
2024-10-08 06:00:39 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/app/rfapplication2/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comapp/rf_application_2/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
